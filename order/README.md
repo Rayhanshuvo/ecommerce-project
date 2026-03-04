5de14c4d15366c32233a8af802f9bc5fcebdec30
Rayhanshuvo <rayhanshuvo.bd@gmail.com>
2026-03-04 15:58:20 +0800

    fix: remove all submodules re-add as normal folders

---
+++ order/README.md
@@ -0,0 +1,14 @@
+## docker build and up
+docker compose down
+docker volume rm ecommerce-project_mysql_data
+docker compose up --build -d
+
+## inside docker
+
+docker exec -it order sh
+
+## log check
+docker compose logs order
+
+## curl request to order service
+curl -X POST http://localhost:8083/api/orders/test
---
+++ order/src/main/java/com/discretesoft/order/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.discretesoft.order.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/test")
+    public ResponseEntity<String> sendTestOrder() {
+        String message = "Test Order: order-" + System.currentTimeMillis();
+        kafkaTemplate.send("order-topic", message);
+        return ResponseEntity.ok("Message sent: " + message);
+    }
+}
